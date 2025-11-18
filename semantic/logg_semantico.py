e7784f4c6162df5a2c2014bc0795416dc9cc16a0
Aepino01 <aepino@espol.edu.ec>
2025-11-18 03:44:17 +0000

    Correccion logs

--- semantic/logg_semantico.py
+++ semantic/logg_semantico.py
@@ -1,6 +1,7 @@
-# Generador de loggs 
+# Generador de logs
 
 import datetime
+import os
 
 errores = []
 
@@ -12,16 +13,23 @@ def agregar_error(mensaje):
 
 
 def guardar_log(usuario_git):
+    # Crear carpetas si no existen
+    ruta_logs = os.path.join("logs", "sem")
+    os.makedirs(ruta_logs, exist_ok=True)
+
+    # Nombre del archivo
     ahora = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
     nombre_archivo = f"semantico-{usuario_git}-{ahora}.txt"
+    ruta_archivo = os.path.join(ruta_logs, nombre_archivo)
 
-    with open(nombre_archivo, "w", encoding="utf-8") as f:
+    # Guardar log
+    with open(ruta_archivo, "w", encoding="utf-8") as f:
         if len(errores) == 0:
             f.write("✔️ No se encontraron errores semánticos.\n")
         else:
             f.write("ERRORES SEMÁNTICOS DETECTADOS:\n")
             f.write("\n".join(errores))
 
-    print(f"[LOG] Archivo generado: {nombre_archivo}")
+    print(f"[LOG] Archivo generado: {ruta_archivo}")
 
     errores.clear()
